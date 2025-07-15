e182b17d1ac714ca5c7a1ca40a28b4bed41c9ac4
Muhammadqodir <km1728.uz@gmail.com>
2025-07-15 14:13:46 +0500

    all to update

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import NewsViewSet
 
 urlpatterns = [
-    path('get_news/', NewsViewSet.as_view({'get': 'get_news'}), name='get_news'),
+    path('get_news/', NewsViewSet.as_view({'get': 'get_news'}), name='get_news_for_news'),
     path('get_news_by_id/<int:pk>/', NewsViewSet.as_view({'get': 'get_news_by_id'}), name='get_news_by_id'),
 ]
